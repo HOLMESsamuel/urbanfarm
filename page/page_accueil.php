df67ad9512a4593d6ea56d9e0268f6d9316a2981
Elise <elise.gabilly@gmail.com>
2019-03-11 17:01:52 +0100

    New layout

--- page/page_accueil.php
+++ page/page_accueil.php
@@ -11,26 +11,18 @@
 
 	<body>
 
-	<div class="row">
-  		<div class="column">
-			<p>azertyuio</p>
-		</div>
-  		<div class="column">
-			<p>azertyuio</p>
-		</div>
-	</div>
-
-		<?php include("elem/elem_menu.php"); ?>
-		<div id="corps">
-			<div id = "contenu">
-				<h2> A propos de nous</h2>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vulputate urna risus, at efficitur magna interdum et. Sed urna mauris, molestie fringilla turpis ut, mollis finibus elit. Proin velit lectus, feugiat id enim et, commodo convallis eros. Donec eros orci, pellentesque in dui ut, gravida auctor lacus. Sed egestas nec enim nec ultricies. Nunc tempor tortor metus, id aliquet dolor facilisis id. In luctus venenatis pulvinar. Ut id nisl elit. Suspendisse tincidunt, nibh et sollicitudin venenatis, lectus est fermentum nisi, nec venenatis lorem eros eu ex. Mauris non nunc quis velit pharetra ornare at et velit. Curabitur at leo porta, tempus nunc at, blandit nulla.
-					Nam vitae efficitur mi. Vivamus pretium augue ut varius ornare. Nunc ac feugiat ipsum. Pellentesque et neque purus. Sed sollicitudin maximus fringilla. Sed dignissim lacus nisi, ut tristique ligula pulvinar vitae. Nulla facilisi. Sed ut felis leo. Etiam vitae dui auctor, egestas urna quis, scelerisque ligula. Morbi maximus mollis est quis venenatis. Cras ac justo pretium, ornare mauris sit amet, gravida enim. Maecenas tincidunt elit quis risus pellentesque, non convallis tortor porttitor.
-				</p>
-			</div>
-	</div>
-	</body>
+	<body>
+<div class="container">
+    <div id="col1">
+        <p>colonnzertyuie 1</p>
+	    <?php include("elem/elem_menu.php"); ?>
+    </div>
+    <div id="col2">
+        <p>colonne 2</p>
+	    <?php include("elem/elem_corp.php"); ?>
+    </div>
+</div>
+</body>
 
 	<footer>
 		<?php include("elem/elem_pied.php"); ?>
