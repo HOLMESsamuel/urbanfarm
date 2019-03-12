2a25ed1bc26df197c9326014f7836745fdd711db
Elise <elise.gabilly@gmail.com>
2019-03-12 12:14:04 +0100

    Working img gallerie

--- page/page_accueil.php
+++ page/page_accueil.php
@@ -13,39 +13,36 @@
 	<body>
 		<div class="container">
 	    	<div id="col1">
-			    	<?php include("elem/elem_menu.php"); ?>
+			    <?php include("elem/elem_menu.php"); ?>
     		</div>
 		    <div id="col2">
-			<p>
-				azertyuiolkjnbvcxcvb
-			</p>
-			<div id="img_slider">
-			<input type="radio" name="images" id="i1" checked>
-			<input type="radio" name="images" id="i2">
-			<input type="radio" name="images" id="i3">
-			<input type="radio" name="images" id="i4">
-			<div class="image" id="one">
-				<img src="img/f1.jpg">
-				<label for="i4" class="precedent">&lt;</label>
-				<label for="i2" class="suivant">&gt;</label>
-			</div>
-			<div class="image" id="two">
-				<img src="img/f2.jpg">
-				<label for="i1" class="precedent">&lt;</label>
-				<label for="i3" class="suivant">&gt;</label>
-			</div>
-			<div class="image" id="three">
-				<img src="img/f3.jpg">
-				<label for="i2" class="precedent">&lt;</label>
-				<label for="i4" class="suivant">&gt;</label>
-			</div>
-			<div class="image" id="four">
-				<img src="img/f4.jpg">
-				<label for="i3" class="precedent">&lt;</label>
-				<label for="i1" class="suivant">&gt;</label>
-			</div>
-		</div>
-
+				<p> azertyuiolkjnbvcxcvb </p>
+				<div id="img_slider">
+					<input type="radio" name="images" id="i1" checked>
+					<input type="radio" name="images" id="i2">
+					<input type="radio" name="images" id="i3">
+					<input type="radio" name="images" id="i4">
+					<div class="image" id="one">
+						<img src="img/f1.jpg">
+						<label for="i4" class="precedent">&lt;</label>
+						<label for="i2" class="suivant">&gt;</label>
+					</div>
+					<div class="image" id="two">
+						<img src="img/f2.jpg">
+						<label for="i1" class="precedent">&lt;</label>
+						<label for="i3" class="suivant">&gt;</label>
+					</div>
+					<div class="image" id="three">
+						<img src="img/f3.jpg">
+						<label for="i2" class="precedent">&lt;</label>
+						<label for="i4" class="suivant">&gt;</label>
+					</div>
+					<div class="image" id="four">
+						<img src="img/f4.jpg">
+						<label for="i3" class="precedent">&lt;</label>
+						<label for="i1" class="suivant">&gt;</label>
+					</div>
+				</div>
 	    	</div>
 		</div>
 	</body>
