bdb99904a4f008307c1bbcf7456972b4277de4b4
Elise <elise.gabilly@gmail.com>
2019-03-12 10:12:45 +0100

    Img on accueil

--- page/page_accueil.php
+++ page/page_accueil.php
@@ -17,7 +17,32 @@
     		</div>
 		    <div id="col2">
 				
-			
+			<div id="img_slider">
+			<input type="radio" name="images" id="i1" checked>
+			<input type="radio" name="images" id="i2">
+			<input type="radio" name="images" id="i3">
+			<input type="radio" name="images" id="i4">
+			<div class="image" id="one">
+				<img src="img/ferme_1.jpg">
+				<label for="i3" class="precedent">&lt;</label>
+				<label for="i2" class="suivant">&gt;</label>
+			</div>
+			<div class="image" id="two">
+				<img src="img/ferme_2.jpg">
+				<label for="i1" class="precedent">&lt;</label>
+				<label for="i3" class="suivant">&gt;</label>
+			</div>
+			<div class="image" id="three">
+				<img src="img/ferme_3.jpg">
+				<label for="i2" class="precedent">&lt;</label>
+				<label for="i4" class="suivant">&gt;</label>
+			</div>
+			<div class="image" id="four">
+				<img src="img/ferme_4.jpg">
+				<label for="i3" class="precedent">&lt;</label>
+				<label for="i1" class="suivant">&gt;</label>
+			</div>
+		</div>
 
 	    	</div>
 		</div>
