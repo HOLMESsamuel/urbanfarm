f4550d3d7f7e6fdcbf3fae21ebb04c816a86faa0
samuel60378 <sam.holmes@live.fr>
2019-03-15 09:42:29 +0100

    Update elem_menu.php
    on peut se connecter

--- page/elem/elem_menu.php
+++ page/elem/elem_menu.php
@@ -12,16 +12,16 @@ try {
 
 if(isset($_POST['valider'])){
 	$mail = htmlspecialchars($_POST['mail']);
-	$mdp = htmlspecialchars($_POST['mdp']);
+	$mdp = sha1($_POST['mdp']);
 
 	if(empty($_POST['mail']) OR empty($_POST['mdp'])){
 		$erreur = "certains champs ne sont pas remplis";
 	} else {
-		$req = $bdd->prepare("SELECT * FROM utilisateur WHERE mail=? AND mdp=?");
+		$req = $bdd->prepare("SELECT * FROM utilisateur WHERE email=? AND motdepasse=?");
 		$req->execute(array($mail,$mdp));
-		$exist = $req-><rowCount();
+		$exist = $req->rowCount();
 		if($exist == 1){
-
+			header('Location: page_profil.php');
 		} else {
 			$erreur = "Le mail ou le mot de passe est incorect";
 		}
