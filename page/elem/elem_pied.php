e75aa4d14609dfd4c782b755569d3e377794c065
Elise <elise.gabilly@gmail.com>
2019-03-22 14:40:08 +0100

    fixe explorer layout

--- page/elem/elem_pied.php
+++ page/elem/elem_pied.php
@@ -1,16 +1,16 @@
-<figure id="image">
+<figure class="con" id="image">
 	<a href="page_contact.php"><img  src = "img/contact.png" height="50" title="Contact"/></a>
 	<figcaption>Contact</figcaption>
 </figure>
-<figure id="image">
+<figure class="act" id="image">
 	<a href="page_actualite.php"><img src = "img/journal.png" height="50" title="Actualites"/></a>
 	<figcaption>Actualites</figcaption>
 </figure>
-<figure id="image">
+<figure class="for" id="image">
 	<a href="page_forum.php"><img src = "img/forum.png" height="45" title="Forum"/></a>
 	<figcaption>Forum</figcaption>
 </figure>
-<figure id="image">
+<figure class="bou" id="image">
 	<a href="page_boutique.php"><img src = "img/boutique.png" height="50" title="Boutique"/></a>
 	<figcaption>Boutique</figcaption>
 </figure>
