bc49a9b47040e9cbb68109d42785496a335d624b
samuel60378 <sam.holmes@live.fr>
2019-03-26 11:30:30 +0100

    modele mvc terminé

---
+++ modele/requeteCapteur.php
@@ -0,0 +1,6 @@
+<?php 
+    function ajoutCapteur(PDO $bdd, String $type, String $etat, int $n°installation){
+        $req = $bdd->prepare("INSERT INTO capteur(type, etat, n°installation) VALUES (?,?,?)");
+        $req->execute(array($type, $etat, $n°installation));
+    }
+?>
