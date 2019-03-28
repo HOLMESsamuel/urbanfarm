62e8002f4bae7a1a688a763dff2d75ea980c8309
Elise <elise.gabilly@gmail.com>
2019-03-28 11:56:15 +0100

    Avencement combobox

--- vue/page_consommation.php
+++ vue/page_consommation.php
@@ -3,14 +3,16 @@
 	<head> <meta charset = utf-8>
 		<title> Urban Farm</title>
 		<link rel = "stylesheet" href = "style/style.css"/>
-		<link rel = "stylesheet" href = "style/style_consomation.css"/>
+		<link rel = "stylesheet" href = "style/style_consommation.css"/>
+
 		<script>
 		window.onload = function () {
 
 		//Better to construct options first and then pass it as a parameter
 		var options = {
 			title: {
-				text: "Spline Chart with Export as Image"
+				text: "Données des capteurs",
+				fontFamily:"Roboto Slab",
 			},
 			animationEnabled: true,
 			exportEnabled: true,
@@ -40,32 +42,24 @@
 		<?php include("elem/elem_entete.php"); ?>
 	</header>	
 	
-	<body>
+	<body onload="createComboBox(5);">
 		<div class="container">
 	    	<div id="col1">
 			    	<?php include("elem/elem_menu.php"); ?>
     		</div>
 		    <div id="col2">
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-				<tr>
-				<td>Owner</td>
-				<td>
-							<select name="owner">
-				<?php 
-				//$sql = mysqli_query($connection, "SELECT username FROM users");
-				//while ($row = $sql->fetch_assoc()){
-				//echo "<option value=\"owner1\">" . $row['username'] . "</option>";				}
-				?>
-				</select>
-				</td>
-				</tr>
-				<div id="chartContainer"></div>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+				
+			<div id="comboBox" >azerty</div>
+			<div id="test">qsdfgyhjhfdfk</div>
+			<div id="chartContainer">huhuhuhuhuhuhuhu</div>				
 			
 	    	</div>
 		</div>
-		<script src="https://canvasjs.com/assets/script/jquery-1.11.1.min.js"></script>
-		<script src="https://canvasjs.com/assets/script/jquery.canvasjs.min.js"></script>
 	</body>
+	<script src="https://canvasjs.com/assets/script/jquery-1.11.1.min.js"></script>
+	<script src="https://canvasjs.com/assets/script/jquery.canvasjs.min.js"></script>
+	<script src="script/script_consommation.js"></script>
 
 	<footer>
 		<?php include("elem/elem_pied.php"); ?>
---
+++ vue/script/script_consommation.js
@@ -0,0 +1,16 @@
+
+var comboBox= document.getElementById("comboBox");
+var graph= document.getElementById("chartContainer");
+
+function createComboBox(n) {
+    alert("Pandicorne !");
+    var liste = document.createElement('select');
+    liste.setAttribute('name', 'liste' );
+    liste.setAttribute('id', 'liste' );
+    for (var i=0; i<5; i++){
+        var optionCapteur = document.createElement('option');
+        optionCapteur.innerHTML = 'capteur '+ i;
+        liste.appendChild(optionCapteur);
+    }
+    comboBox.appendChild(liste);
+}
