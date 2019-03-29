52661df1be49ad10e3190c3d9a6266776e8ba3be
Frank Guoi <frank.guoi@hotmail.fr>
2019-03-29 16:32:05 +0100

    boutique
    produits specimen de la boutique et style

--- vue/page_boutique.php
+++ vue/page_boutique.php
@@ -3,7 +3,7 @@
 	<head> <meta charset = utf-8>
 		<title> Urban Farm</title>
 		<link rel = "stylesheet" href = "style/style.css"/>
-		<link rel = "stylesheet" href = "style/style_boutique.css"/>
+		<link rel = "stylesheet" href = "style/style_boutiquee.css"/>
 	</head>
 	<header>
 		<?php include("elem/elem_entete.php"); ?>
@@ -14,9 +14,20 @@
 	    	<div id="col1">
 			    	<?php include("elem/elem_menu.php"); ?>
     		</div>
-		    <div id="col2">
-				
-			
+			<div id="col2">
+
+				<div class="produit">
+					<h1>Lorem Ipsum</h1>
+					<p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					<h2>00,00€</h2>
+				</div>
+
+				<div class="produit">
+					<h1>Lorem Ipsum</h1>
+					<p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					<h2>00,00€</h2>
+				</div>
+
 	    	</div>
 		</div>
 	</body>
