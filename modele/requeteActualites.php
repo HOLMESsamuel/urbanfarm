2dabfea52dbc8fc8752c79ec6412d7ecdbc7876b
LouiseNdiaye <LouiseNdiaye@users.noreply.github.com>
2019-04-02 18:33:54 +0200

    actu

--- modele/requeteActualites.php
+++ modele/requeteActualites.php
@@ -1,24 +1,38 @@
 <?php
     include("connexion.php");
 
-    function recupereTitre(PDO $bdd){
-        $req=$bdd->prepare("SELECT titre FROM article ORDER BY date");
-        $req->excute;
-    }
-
-    function recupereDate(PDO $bdd){
-        $req=$bdd->prepare("SELECT date FROM article");
-        $req->excute;
-    }
-    function recupereArticle(PDO $bdd){
-        $req="SELECT * FROM article";
+    function recupereArticle(PDO $bdd, String $titre){
+        $req="SELECT contenu FROM article WHERE titre=.$titre.";
         $resultat=$bdd->query($req);
         while ($row=$resultat->fetch()){
             echo "<a href='titre'>".$row['titre']."</a>";
-            echo " ";
-            echo "<a href='date'>".$row['date']."</a>";
-            echo "<br>";
         }
+    }
+
+    function recupereTitre(PDO $bdd){
+        $req="SELECT * FROM article";
+        $resultat=$bdd->query($req);
+        $n=0;
+        while ($row=$resultat->fetch()){
+            $n=$n+1;
+            echo "<a class='titre' href='titre.$n'>".$row['titre']."</a>";
+            echo " ";
+            echo "<a class='date'>".$row['date']."</a>";
+            echo "<br>";
+            echo "<a class='contenu'>".substr($row['contenu'],0,100)."</a>";
+            echo "... <br>";
+        }
+    }
+
+    function dernierTitre(PDO $bdd){
+        $req="SELECT COUNT(*) FROM article ";
+        $dernierTitre=$bdd->query($req);
+        $row=$dernierTitre->fetch();
+        echo $row;
+        $req="SELECT titre FROM article WHERE n°article=.$row";
+        $resultat=$bdd->query($req);
+        $row=$resultat->fetch();
+        echo $row;
     }
 
 ?>
