4f1ee98d5fb4f0a7e4e178980908f4f4bf38e567
HOLMESsamuel <sam.holmes@live.fr>
2019-04-04 09:16:58 +0200

    ajax contact

---
+++ controleur/ct_contact.php
@@ -0,0 +1,12 @@
+<?php 
+session_start();
+if(isset($_SESSION['mail'])){
+    $mail = htmlspecialchars($_POST['mail']);
+    $titre = htmlspecialchars($_POST['titre']); 
+    $texte = htmlspecialchars($_POST['texte']); 
+} else {
+    echo "Il faut être connecté pour envoyer un message";
+}
+
+
+?>
