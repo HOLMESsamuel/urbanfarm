56041c23d2bdfacf28fa582ac9f15242bdaaaf4f
Elise <elise.gabilly@gmail.com>
2019-04-04 12:01:13 +0200

    entete deco sur le clic

--- vue/elem/elem_entete.php
+++ vue/elem/elem_entete.php
@@ -1,1 +1,19 @@
-<a href="page_accueil.php"><img src = "img/logo.png" height="100" alt="logo"/></a>
+<?php 
+session_start();
+if(isset($_SESSION['mail'])){
+  echo '<span id="btn_accueil" onclick="comfirmDeco();"> <img src = "img/logo.png" height="100" alt="logo"/> </span>';
+} else {
+  echo '<span id="btn_accueil"> <img src = "img/logo.png" height="100" alt="logo"/> </span>';
+}
+?>
+
+<script>
+function comfirmDeco(){
+  var txt;
+  var r = confirm("Es tu sur de vouloir continuer ? Retourner a l'accueil vas te déconneter ");
+  if(r==true && ){
+    document.location.href = "page_accueil.php";
+  }
+}
+</script>
+
