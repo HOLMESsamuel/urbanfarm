d2bc669ef56ab98dc2d7c8e429b06b34d838bd15
Elise <elise.gabilly@gmail.com>
2019-04-09 08:35:25 +0200

    test recherche bar

--- vue/page_actualite.php
+++ vue/page_actualite.php
@@ -15,6 +15,62 @@
 		<div class="container">
 	    <div id="col1">
 			  <?php include("elem/elem_menu.php"); ?>
+				
+				<?php
+
+					// php search data in mysql database using PDO
+					// set data in input text
+
+					$world = "";
+
+					if(isset($_POST['Find']))
+					{
+									// connect to mysql
+							try {
+									$pdoConnect = new PDO("mysql:host=localhost;dbname=test_db","root","");
+							} catch (PDOException $exc) {
+									echo $exc->getMessage();
+									exit();
+							}
+							
+							// id to search
+							$id = $_POST['id'];
+							
+							// mysql search query
+							$pdoQuery = "SELECT * FROM users WHERE id = :id";
+							
+							$pdoResult = $pdoConnect->prepare($pdoQuery);
+							
+							//set your id to the query id
+							$pdoExec = $pdoResult->execute(array(":id"=>$id));
+							
+							if($pdoExec)
+							{
+											// if id exist 
+											// show data in inputs
+									if($pdoResult->rowCount()>0)
+									{
+											foreach($pdoResult as $row)
+											{
+													$word = $row['word'];
+											}
+									}
+											// if the id not exist
+											// show a message and clear inputs
+									else{
+											echo 'No Data With This word';
+									}
+							}else{
+									echo 'ERROR Data Not Inserted';
+							}
+					}
+					?>
+
+					<form action="php_search_in_mysql_database_using_pdo.php" method="post">
+						Word To Search : <input type="text" name="word" value="<?php echo $word;?>"><br><br>
+						<input type="submit" name="Find" value="Find Data">
+					</form>
+
 			</div>
 
 			<div id="col2">
