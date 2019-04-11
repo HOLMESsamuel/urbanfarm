db795f242072aadfc56b5a83507e52acc13311bd
Melody-ss <48448196+Melody-ss@users.noreply.github.com>
2019-04-11 09:48:39 +0200

    csb

--- vue/page_forum.php
+++ vue/page_forum.php
@@ -15,7 +15,38 @@
 			    	<?php include("elem/elem_menu.php"); ?>
     		</div>
 		    <div id="col2">
-				/* Complete the main part here */	
+				/
+	
+
+<body>
+  <div id="questionlist">
+	<div id="header">
+		<h1>questions</h1>
+	</div>
+	<div class="content">
+	
+				<h3>1.Question1?</h3>
+				<p>Réponse1.</p>
+
+				<h3>2.Question2?</h3>
+				<p>Réponse2.</p>
+		
+				<h3>3.Question3?</h3>
+				<p>Réponse3.</p>
+
+				<h3>4.Question4?</h3>
+				<p>Réponse4.</p>
+
+				<h3>5.Question5?</h3>
+				<p>Réponse5.</p>
+
+				<h3>6.Question6?</h3>
+				<p>Réponse6.</p>
+			</div>					
+  </div>
+</body>
+</html>
+/	
 
 	    	</div>
 		</div>
