5e3f757a5ca91435eece5765c11bc784b3b7df7a
Frank Guoi <frank.guoi@hotmail.fr>
2019-04-16 18:14:19 +0200

    boutique
    revue de la répartition du code entre M, V et C

--- modele/requeteBoutique.php
+++ modele/requeteBoutique.php
@@ -1,33 +1,6 @@
 <?php
 include 'connexion.php';
 
-/*
- * recupere et affiche les informations d'un produit de la boutique
- */
-function recupereProduit(PDO $bdd)
-{
-    $req = $bdd->query('SELECT * FROM produit');
-    while ($produit = $req->fetch()) {
-        echo
-            '<div class="box_product"> 
-                    <h1>' . $produit['type'] . '</h1>
-                    <h3 class="ref"> ref ' . $produit['n°produit'] . '</h3>
-                    <div class="information">
-                        <div class="photo">
-                        </div>
-                        <p class="description">' . $produit['description'] . '</p>
-                        <div class="achat">
-                            <h2>' . $produit['prix'] . '€</h2>
-                            <form method="post" action="">
-                                <label>
-                                    unités<input type="number" name="number" id="number" min="1" max="99" step="1" value="1">
-                                </label>
-                                <input type="submit" id="submit" value="Ajouter au panier">
-                            </form>
-                        </div>
-                    </div>
-                </div>';
-    }
-}
+$req = $bdd->query('SELECT * FROM produit');
 
 ?>
--- vue/page_boutique.php
+++ vue/page_boutique.php
@@ -17,7 +17,27 @@
         <?php include("elem/elem_menu.php"); ?>
     </div>
     <div id="col2">
-        <?php recupereProduit($bdd); ?>
+        <?php while ($produit = $req->fetch()) {
+            echo
+                '<div class="box_product"> 
+                    <h1>' . $produit['type'] . '</h1>
+                    <h3 class="ref"> ref ' . $produit['n°produit'] . '</h3>
+                    <div class="information">
+                        <div class="photo">
+                        </div>
+                        <p class="description">' . $produit['description'] . '</p>
+                        <div class="achat">
+                            <h2>' . $produit['prix'] . '€</h2>
+                            <form method="post" action="">
+                                <label>
+                                    unités<input type="number" name="number" id="number" min="1" max="99" step="1" value="1">
+                                </label>
+                                <input type="submit" id="submit" value="Ajouter au panier">
+                            </form>
+                        </div>
+                    </div>
+                </div>';
+        } ?>
     </div>
 </div>
 </body>
