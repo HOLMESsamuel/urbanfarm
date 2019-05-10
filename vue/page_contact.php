a4ab4090fda1944bdbf1cb39379524883fdaa095
HOLMESsamuel <sam.holmes@live.fr>
2019-05-10 14:58:57 +0200

    envoie de mail

--- vue/page_contact.php
+++ vue/page_contact.php
@@ -82,7 +82,7 @@
 					<br>
 					<textarea  id="text" placeholder="Ecrivez votre message ici ..."></textarea>
 					<br>
-					<input type="submit"  onclick="envoiMessage();" id="envoiMessage" name="envoi" value="Envoyer"/>
+					<input type="submit" id="envoiMessage" name="envoi" value="Envoyer"/>
 				</form>
 				<p id="erreur"></p>
 			</div>
