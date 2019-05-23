019066b3a714a4c7a7563820543feaa56ba15f89
Elise <elise.gabilly@gmail.com>
2019-05-23 09:14:48 +0200

    Update index.php
    Co-Authored-By: EliseGabilly <elisegabilly@users.noreply.github.com>

--- index.php
+++ index.php
@@ -11,13 +11,10 @@ include("modele/connexion.php");
 if (!empty($_GET['page']) && is_file('vue/page_'.$_GET['page'].'.php'))
 {
         include 'vue/page_'.$_GET['page'].'.php';
-}
-else
-{
+} else {
         session_destroy();
         session_start();
         include 'vue/page_accueil.php';
-        
 }
  
 
