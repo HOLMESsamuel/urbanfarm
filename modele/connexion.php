0eebee3663319a967f94ceaa070b9b75fae7293f
Elise <elise.gabilly@gmail.com>
2019-05-28 14:53:25 +0200

    Logo poussin

--- modele/connexion.php
+++ modele/connexion.php
@@ -1,7 +1,9 @@
 <?php
 try {
-	$bdd = new PDO('mysql:host=localhost;dbname=urbanfarm;charset=utf8', 'root', '');
-	$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$bdd = new PDO('mysql:host=localhost;
+		dbname=urbanfarm;charset=utf8', 'root', '');
+	$bdd->setAttribute(PDO::ATTR_ERRMODE, 
+		PDO::ERRMODE_EXCEPTION);
 	$bdd->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 	
 } catch(Exception $e) {
