7732c2136faecf88f427c63b868d54dea932a09c
Elise <elise.gabilly@gmail.com>
2019-05-31 13:49:17 +0200

    logo annime

--- vue/elem/elem_entete.php
+++ vue/elem/elem_entete.php
@@ -2,14 +2,14 @@
  if (isset($_SESSION['mail'])) { 
   echo '<span id="deconnecte" onclick="retour();"><img src = "vue/img/deconnect.png" height="20"/></span>';
   echo '<span id="btn_accueil" onclick="comfirmDeco();"> <img src = "vue/img/logo.png" 
-    onmouseover="this.src=\'vue/img/logoPoussin.png\';" onmouseout="this.src=\'vue/img/logo.png\';" 
+    onmouseover="this.src=\'vue/img/logoPoussin.gif\';" onmouseout="this.src=\'vue/img/logo.png\';" 
     height="100" alt="logo"/> </span>';
   if (strpos(basename($_SERVER['PHP_SELF']), 'admin') == false){
     echo '<span id="panier"><img src = "vue/img/boutique.png" height="30"/></span>';
   }
 } else {
   echo '<span id="btn_accueil" onclick="retour();"> <img src = "vue/img/logo.png" 
-    onmouseover="this.src=\'vue/img/logoPoussin.png\';" onmouseout="this.src=\'vue/img/logo.png\';" 
+    onmouseover="this.src=\'vue/img/logoPoussin.gif\';" onmouseout="this.src=\'vue/img/logo.png\';" 
     height="100" alt="logo"/> </span>';
 }
 
