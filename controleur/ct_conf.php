58f164c7097450b1f766159d3027b6daa558770d
Elise <elise.gabilly@gmail.com>
2019-06-12 11:31:05 +0200

    page conf

--- controleur/ct_conf.php
+++ controleur/ct_conf.php
@@ -10,10 +10,18 @@ if (!empty($_POST["code"])) {
     $code = test_input($_POST["code"]);
 }
 
+var_dump($input_mail);
+var_dump($code);
 
 if(!empty($_POST['input_mail']) AND !empty($_POST['code'])){
-	echo "Blop";
+	valMail ($bdd, $input_mail, $code);
 } else {
 	echo "Tous les champs doivent être remplis";
 }
-?>
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
